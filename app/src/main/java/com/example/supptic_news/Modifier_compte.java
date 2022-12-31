716ae2658c655f9f5743a20fa32534eb5689c41b
Dell <araboyoussoufa06@gmail.com>
2022-12-31 08:37:43 +0100

    ici j'ai ajouté l'activity modification_compte, dans color une nouvelle mise à jour de la couleur rouge

---
+++ app/src/main/java/com/example/supptic_news/Modifier_compte.java
@@ -0,0 +1,14 @@
+package com.example.supptic_news;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Modifier_compte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modifier_compte);
+    }
+}
